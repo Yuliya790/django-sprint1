264f3a8061c433a8cafa97391922c1b6049a3c7e
Yuliya790 <tushina.yuliya@mail.ru>
2025-10-02 02:36:20 -0700

    Initial commit: Django blog project

---
+++ blogicum/urls.py
@@ -0,0 +1,10 @@
+
+# blogicum/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+]
